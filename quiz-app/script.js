0145a19a7c8e4481ca0c984e71700e54b45cbf36
KSJ <fly881004@gamil.com>
2022-09-13 15:30:54 +0900

    completed second project

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -6,21 +6,21 @@ const quizData = [
         c: '20',
         d: '25',
         correct: 'c'
-    },{
+    }, {
         question: 'What is the best programming language?',
-        a:'Java',
+        a: 'Java',
         b: 'C',
-        c:'python',
-        d:'JavaScript',
-        correct:'d'
-    },{
+        c: 'python',
+        d: 'JavaScript',
+        correct: 'd'
+    }, {
         question: 'Who is he President of US?',
         a: 'Florin Pop',
         b: 'Donald Trump',
         c: 'Ivan Saldano',
         d: 'Mihai Andrei',
-        correct:'b'
-    },{
+        correct: 'b'
+    }, {
         question: 'What does HTML stand for?',
         a: 'Hypertext Markup Language',
         b: 'Cascading Style Sheet',
@@ -30,17 +30,21 @@ const quizData = [
     }
 ];
 const questionEl = document.getElementById('question');
+const answerEls = document.querySelectorAll('.answer');
+const quiz = document.getElementById('quiz');
 const a_text = document.getElementById('a-text');
 const b_text = document.getElementById('b-text');
 const c_text = document.getElementById('c-text');
 const d_text = document.getElementById('d-text');
 const submitBtn = document.getElementById('submit');
 
+
 let currentQuiz = 0;
+let score = 0;
 loadQuiz();
 
-function loadQuiz(){
-    
+function loadQuiz() {
+    deselectAnswers();
     const currentQuizData = quizData[currentQuiz];
 
     questionEl.innerText = currentQuizData.question;
@@ -50,14 +54,39 @@ function loadQuiz(){
     d_text.innerText = currentQuizData.d;
 }
 
-submitBtn.addEventListener("click",()=>{
-    
-    currentQuiz++;
-    if(currentQuiz< quizData.length){
-        loadQuiz();
-    }
-    else{
-        // TODO: show results
-        alert("You finished!");
+function getSelected() {
+    let answer = undefined;
+    console.log(answer);
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            console.log(answerEl);
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+function deselectAnswers(){
+    answerEls.forEach((answerEl)=>{
+            answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if(answer === quizData[currentQuiz].correct )
+        {
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        }
+        else {
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>`;
+        }
     }
 });
